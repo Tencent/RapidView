7ae7beee2fc2b8341a0a8a18d5028bbdef9e56e5
Earlonus <earlonus@vip.qq.com>
2017-10-30 13:39:09 +0800

    add license and delete redundancy files
    add license and delete redundancy files

--- RapidViewDemo/app/src/main/java/com/tencent/rapidview/utils/Closer.java
+++ RapidViewDemo/app/src/main/java/com/tencent/rapidview/utils/Closer.java
@@ -1,24 +1,16 @@
-/**  
+/***************************************************************************************************
+ Tencent is pleased to support the open source community by making RapidView available.
+ Copyright (C) 2017 THL A29 Limited, a Tencent company. All rights reserved.
+ Licensed under the MITLicense (the "License"); you may not use this file except in compliance
+ withthe License. You mayobtain a copy of the License at
 
- * Copyright © 2014Tencent. All rights reserved.
+ http://opensource.org/licenses/MIT
 
- *
-
- * @Title: Closer.java
-
- * @Prject: MobileAssistant_trunk
-
- * @Package: com.tencent.assistant.utils
-
- * @Description: 简化流关闭操作，不用每处地方都写个try...catch
-
- * @author: jieshao  
-
- * @date: 2014-6-20 下午6:01:17
-
- * @version: V1.0  
-
- */
+ Unlessrequired by applicable law or agreed to in writing, software distributed under the License is
+ distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ implied. See the License for the specific language governing permissions and limitations under the
+ License.
+ ***************************************************************************************************/
 package com.tencent.rapidview.utils;
 
 import java.io.Closeable;
--- rapidview/utils/FileUtil.java
+++ rapidview/utils/FileUtil.java
@@ -1,3 +1,16 @@
+/***************************************************************************************************
+ Tencent is pleased to support the open source community by making RapidView available.
+ Copyright (C) 2017 THL A29 Limited, a Tencent company. All rights reserved.
+ Licensed under the MITLicense (the "License"); you may not use this file except in compliance
+ withthe License. You mayobtain a copy of the License at
+
+ http://opensource.org/licenses/MIT
+
+ Unlessrequired by applicable law or agreed to in writing, software distributed under the License is
+ distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ implied. See the License for the specific language governing permissions and limitations under the
+ License.
+ ***************************************************************************************************/
 package com.tencent.rapidview.utils;
 
 import java.io.ByteArrayOutputStream;
@@ -271,7 +284,7 @@ public class FileUtil
         return readFile(dest,baos,null);
     }
 
-	public static boolean readFile(String dest, ByteArrayOutputStream baos, ByteArrayPool byteArrayPool)
+	public static boolean readFile(String dest, ByteArrayOutputStream baos, ByteArrayUtils byteArrayPool)
 	{
 		File file = new File(dest);
 		if (null == baos || file.length() == 0)
