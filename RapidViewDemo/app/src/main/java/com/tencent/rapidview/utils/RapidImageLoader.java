af0f0a50fa58da2a4a06a333faf9f415561f598b
Earlonus <earlonus@vip.qq.com>
2017-10-30 14:02:04 +0800

    update license
    update license

--- RapidViewDemo/app/src/main/java/com/tencent/rapidview/utils/RapidImageLoader.java
+++ RapidViewDemo/app/src/main/java/com/tencent/rapidview/utils/RapidImageLoader.java
@@ -7,7 +7,7 @@ package com.tencent.rapidview.utils;
 
  http://opensource.org/licenses/MIT
 
- Unlessrequired by applicable law or agreed to in writing, software distributed under the License is
+ Unless required by applicable law or agreed to in writing, software distributed under the License is
  distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
  implied. See the License for the specific language governing permissions and limitations under the
  License.
