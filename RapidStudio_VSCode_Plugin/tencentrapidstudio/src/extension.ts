2ee4a6d1f864b982f814598f765409a40f24b42e
YongdongHe <heyongdonghe@qq.com>
2017-11-02 15:59:28 +0800

    [add] add menu for command refresh

--- RapidStudio_VSCode_Plugin/tencentrapidstudio/src/extension.ts
+++ RapidStudio_VSCode_Plugin/tencentrapidstudio/src/extension.ts
@@ -1,11 +1,10 @@
 'use strict';
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
-
+import {window, commands, Disposable, ExtensionContext, StatusBarAlignment, StatusBarItem, TextDocument} from 'vscode';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export function activate(context: ExtensionContext) {
 
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
@@ -14,15 +13,45 @@ export function activate(context: vscode.ExtensionContext) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
+    let disposable = commands.registerCommand('extension.sayHello', () => {
         // The code you place here will be executed every time your command is executed
-
+        var msg = "Hello Rapid Studio";
         // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+        window.showInformationMessage("Hello Rapid Studio");
+        let adbUtils = new ADBUtils();
+        adbUtils.sendADBCommand("echo hello rapid studio");
+
     });
 
+    let refreshFileTask = commands.registerCommand('extension.refreshFile', () => {
+        // The code you place here will be executed every time your command is executed
+        window.showInformationMessage("Refreshing File");
+
+    });
+
+    let refreshProjectTask = commands.registerCommand('extension.refreshProject',()=>{
+        window.showInformationMessage("Refreshing Project");
+    })
+
     context.subscriptions.push(disposable);
+    context.subscriptions.push(refreshFileTask);
+    context.subscriptions.push(refreshProjectTask);
 }
+class ADBUtils {
+        public sendADBCommand(cmdStr : String) {
+    
+            // Create as needed
+            var exec = require('child_process').exec;
+            exec(cmdStr, function(err,stdout,stderr){
+                if(err) {
+                    console.log('Adb runtime error: '+stderr);
+                } else {
+                    console.log(stdout);
+                }
+            });
+        }
+}
+
 
 // this method is called when your extension is deactivated
 export function deactivate() {
