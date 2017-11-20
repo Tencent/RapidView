34b82bd93423010af7937ff0a6384d1309b71061
YongdongHe <heyongdonghe@qq.com>
2017-11-20 15:31:15 +0800

    [Fix] fix save command for untitled file

--- rapidstudio_vscode_extension/rapidstudio/src/command/save.ts
+++ rapidstudio_vscode_extension/rapidstudio/src/command/save.ts
@@ -7,6 +7,11 @@ export class SavaRapidFileCommand implements RapidCommand{
     public execute(...args: any[]):any{
         try{
             let autoSync = workspace.getConfiguration("rapidstudio").get<Boolean>('autoSync');
+            if(window.activeTextEditor.document.isUntitled){
+                commands.executeCommand("workbench.action.files.saveAs");
+                return;
+            }
+
             console.log(autoSync);
             window.activeTextEditor.document.save();
             if(autoSync === true){
